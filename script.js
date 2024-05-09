ce7b60dfd5aa772d8aac545e76670d4c890ca1ec
Pranjuljain1602 <pranjuljain54@gmail.com>
2024-05-09 10:47:22 +0530

    footer animation added line number 260

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ function loadingAnimation() {
     var tl = gsap.timeline();
 
     // Add GSAP animations
-    tl.from(".isro-text", { opacity: 0, duration: 1, delay: 1 })
-      .from(".rocket", { y: "-100vh", ease: "bounce.out", duration: 1 }, "-=0.5")
+    tl.from(".rocket", { y: "-100vh", ease: "bounce.out", duration: 1 }, "-=0.5")
       .from(".cloud1, .cloud2, .cloud3", { y: "-100vh", duration: 1, stagger: 0.5 }, "-=0.5")
-      .to(".preloader", { opacity: 0, duration: 0.5, delay: 1, onComplete: showWebsiteContent });
+      .to(".preloader", { opacity: 0, y: "-90vh",duration: 1, delay: 1, onComplete: showWebsiteContent });
 
     // Function to show website content after preloader animation completes
     function showWebsiteContent() {
@@ -19,7 +18,6 @@ function loadingAnimation() {
     // Additional animations
     tl.from("#page1", {
         delay: 0.17,
-        y: -100,
         opacity: 0,
         duration: 0.4,
         ease: "power4.out"
@@ -256,6 +254,66 @@ const changeSlide = (slideNumber) => {
         }
     )
 }
+
+
+
+// GSAP timeline for the footer animation
+var footerAnimation = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#footer-top", // Trigger when the footer comes into view
+    //   markers: true,
+      start: "top 80%", // Start animation when footer is 80% into view
+      end: "top 20%", // End animation when footer is 70% into view
+      scrub: 3 // Smooth scrubbing effect
+    }
+  });
+  
+  // Define animations for different elements in the footer
+  footerAnimation
+    // Animation for #footer-top elements
+    .from("#footer-top img, #footer-top h1", {
+      opacity: 0,
+      y: -20,
+      stagger: 0.2
+    }, '+=0.5')
+    // Animation for #footer-content elements
+    .from("#footer-content p, #footer-content ul", {
+      opacity: 0,
+      y: -20,
+      stagger: 0.2
+    })
+    // Animation for #footer-bottom elements
+    .from("#footer-bottom h4", {
+      opacity: 0,
+      y: -20
+    });
+  
+  // Add span elements to the h1 element in the footer
+  var footerH1 = document.querySelector("#footer-top h1");
+  var clutter = "";
+  footerH1.textContent.split("").forEach(function (letter) {
+    clutter += `<span>${letter}</span>`;
+  });
+  footerH1.innerHTML = clutter;
+  
+  // GSAP animation for list items on hover
+  document.querySelectorAll("#footer-content ul li").forEach(function (li) {
+    li.addEventListener("mouseenter", function () {
+      gsap.to(li, {
+        scale: 1.2,
+        duration: 0.3
+      });
+    });
+    li.addEventListener("mouseleave", function () {
+      gsap.to(li, {
+        scale: 1,
+        duration: 0.3
+      });
+    });
+  });
+  
+  
+  
 
 
 
